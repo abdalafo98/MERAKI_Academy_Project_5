3c57ae859f2b92418353ebb9a78ea24de5e8f461
user name <abdalafo.abubaker.com>
2021-06-27 15:48:51 +0300

    add addToBreakfast to foodTraker

--- backend/routers/controllers/foodTracker.js
+++ backend/routers/controllers/foodTracker.js
@@ -22,6 +22,16 @@ const addToSnack = (req, res) => {
   });
 };
 
+const addToLunch = (req, res) => {
+  const user_id = req.token.id;
+  const { name } = req.body;
+  const query = `INSERT INTO lunch (name, user_id) VALUES (?,?)`;
+  const data = [name, user_id];
+  db.query(query, data, (err, result) => {
+    if (err) res.status(500).send("insert is not done");
+    res.status(200).send("insert is done");
+  });
+};
 const createFoodTracker = (req, res) => {
   const user_id = req.token.id;
   const { breakfast, snack, lunch, dinner, glassesOfWater, activeTime } =
